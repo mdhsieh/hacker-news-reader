35cb6fc720e0d4f8ec35332a62aca7ae1d940793
Michael Hsieh <mdhsieh8@gmail.com>
2022-09-10 09:52:03 -0700

    feat: Get 100 news articles instead of 10.

--- HackerNews/NewsViewModel.swift
+++ HackerNews/NewsViewModel.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 class NewsViewModel: ObservableObject {
-	@Published var stories: [Item?] = Array(repeating: nil, count: 10)
+	@Published var stories: [Item?] = Array(repeating: nil, count: 100)
 	
 	func fetchTopStories() {
 		let url = URL(string: "https://hacker-news.firebaseio.com/v0/beststories.json")!
 		let request = APIRequest(url: url)
 		request.perform { [weak self] (ids: [Int]?) -> Void in
-			guard let ids = ids?.prefix(10) else { return }
+			guard let ids = ids?.prefix(100) else { return }
 			for (index, id) in ids.enumerated() {
 				self?.fetchStory(withID: id) { story in
 					self?.stories[index] = story
