abd8dc02fee6e2970ab8cadf841d084cfc967e92
Michael Hsieh <mdhsieh8@gmail.com>
2022-09-12 10:30:00 -0700

    feat: Query top 100 news articles from front page.
    Algolia HN API displayed more articles, but did not display 100 articles.

--- Hacker News/Models/NetworkManager.swift
+++ Hacker News/Models/NetworkManager.swift
@@ -16,7 +16,7 @@ class NetworkManager: ObservableObject {
     @Published var posts = [Post]()
     
     func fetchData() {
-        if let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page") {
+        if let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page&hitsPerPage=100") {
             let session = URLSession(configuration: .default)
             let task = session.dataTask(with: url) { data, response, error in
                 if (error == nil) {
