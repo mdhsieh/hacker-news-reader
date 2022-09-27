447bde58e3d132f5116459703325870b69d3873c
Michael Hsieh <mdhsieh8@gmail.com>
2022-09-27 16:07:15 -0700

    fix: Remove pink color which is identical to red. Instead of using opacity to show a color is selected, show a border around selected color.
    Border is white to stand out more in dark mode, and red in light mode.

--- HackerNews/View/CustomColorPicker.swift
+++ HackerNews/View/CustomColorPicker.swift
@@ -9,22 +9,54 @@ import SwiftUI
 
 struct CustomColorPicker: View {
     @Binding var selectedColor: Color
-    private let colors: [Color] = [.teal, .green, .orange, .purple, .indigo, .blue, .yellow, .pink, .red]
+    private let colors: [Color] = [.teal, .green, .orange, .purple, .indigo, .blue, .yellow, .pink]
     var colorData: ColorData
+    
+    @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
         ScrollView(.horizontal) {
             HStack {
                 ForEach(colors, id: \.self) { color in
-                    Circle().foregroundColor(color)
-                        .frame(width: 45, height: 45)
-                        .opacity(color == selectedColor ? 0.5 : 1.0)
-                        .scaleEffect(color == selectedColor ? 1.1 : 1.0)
-                        .onTapGesture {
-                            selectedColor = color
-                            print("Changed color to \(selectedColor.description)")
-                            colorData.saveColor(color: selectedColor)
-                        }
+                    
+                    if (color == selectedColor && colorScheme == .dark) {
+                        // White stroke circle in dark mode
+                        Circle()
+                            .stroke(Color.white, lineWidth: 4)
+                            .background(Circle().fill(color))
+                            .frame(width: 45, height: 45)
+                            .scaleEffect(color == selectedColor ? 1.1 : 1.0)
+                            .onTapGesture {
+                                selectedColor = color
+                                print("Changed selected color in dark mode to \(selectedColor.description)")
+                                colorData.saveColor(color: selectedColor)
+                            }
+                    } else if (color == selectedColor) {
+                        // Dark colored stroke circle in light mode
+                        Circle()
+                            .stroke(Color.red, lineWidth: 4)
+                            .background(Circle().fill(color))
+                            .frame(width: 45, height: 45)
+                            .scaleEffect(color == selectedColor ? 1.1 : 1.0)
+                            .onTapGesture {
+                                selectedColor = color
+                                print("Changed selected color in light mode to \(selectedColor.description)")
+                                colorData.saveColor(color: selectedColor)
+                            }
+                    }
+                    else {
+                        // Otherwise show a circle in that color
+                        Circle()
+                            .foregroundColor(color)
+                            .frame(width: 45, height: 45)
+                            .scaleEffect(color == selectedColor ? 1.1 : 1.0)
+                            .onTapGesture {
+                                selectedColor = color
+                                print("Changed color to \(selectedColor.description)")
+                                colorData.saveColor(color: selectedColor)
+                            }
+                    }
+                    
                 }
             }
             .padding()
